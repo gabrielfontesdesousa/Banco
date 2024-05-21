6699bc3ed5fa0d4b7929915b894ff513f5a6850b
gabriel fontes de sousa <gabriel.sousa235@etec.sp.gov.br>
2024-05-21 00:03:51 -0300

    concluindo atividade adicionando sistema de manipulção de vetores de objetos e organizando as filas de acordo com a atividade

--- Banco/Cliente.cs
+++ Banco/Cliente.cs
@@ -1,160 +1,155 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace aplicativoBanco
+namespace Banco
 {
-    public class Cliente
+    internal class Cliente
     {
+        Cliente[] p1;
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string rg { get; set; }
+        public string cpf { get; set; }
 
+        private int clientesEmFilas = 0;
+        private int prioridades = 0;
 
-   
-        public int _idCliente;
-        private static List<int> idsCadastrados = new List<int>();
-        public int idCliente
+        public Cliente(int id = 0, string nome = "", string rg = "", string cpf = "")
         {
-            get
+            p1 = new Cliente[10];
+            this.id = id;
+            this.nome = nome;
+            this.rg = rg;
+            this.cpf = cpf;
+
+        }
+        public void CadastrarCliente()
+        {
+            if (clientesEmFilas < 10)
             {
-                return _idCliente;
+                Console.WriteLine("\nInsira o id do cliente: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o Nome do cliente");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Insira seu RG:");
+                string rg = Console.ReadLine();
+                Console.WriteLine("Insira seu CPF");
+                string cpf = Console.ReadLine();
+                Cliente c1 = new Cliente(id, nome, rg, cpf);
+                p1[clientesEmFilas] = c1;
+                clientesEmFilas++;
             }
-            set
+            else
             {
-                
-                    if (!idsCadastrados.Contains(value) && value > 0)
+                throw new Exception("Impossivel inserir,Fila cheia");
+            }
+
+
+
+        }
+
+        public void InserirPrioritarios()
+        {
+            if (clientesEmFilas < 10)
+            {
+
+                Console.WriteLine("\nInsira o id do cliente: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o Nome do cliente");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Insira seu RG:");
+                string rg = Console.ReadLine();
+                Console.WriteLine("Insira seu CPF");
+                string cpf = Console.ReadLine();
+                Cliente prioridade = new Cliente(id, nome, rg, cpf);
+                for (int i = clientesEmFilas - 1; i >= prioridades; i--)
+                {
+                    p1[i + 1] = p1[i];
+                }
+                p1[prioridades] = prioridade;
+                clientesEmFilas++;
+                prioridades++;
+            }
+            else
+            {
+                Console.WriteLine("Fila Cheia:");
+            }
+        }
+
+        public void ListarClientes()
+        {
+            Console.Clear();
+
+            if (clientesEmFilas > 0)
+            {
+
+
+                for (int i = 0; i < prioridades; i++)
+                {
+                    if (prioridades > 0)
                     {
-                        _idCliente = value;
-                        idsCadastrados.Add(value);
+
+                        if (p1[i] != null)
+                        {
+                            Console.WriteLine("\nPacientes Prioritários:");
+                            Console.WriteLine($"Paciente:");
+                            Console.WriteLine($"ID:{p1[i].id}");
+                            Console.WriteLine($"Nome:{p1[i].nome}");
+                            Console.WriteLine($"CPF:{p1[i].cpf}");
+                            Console.WriteLine($"RG:{p1[i].rg}");
+                        }
+
                     }
-                    else
+                }
+
+                Console.WriteLine("\nPacientes não prioritários:");
+                for (int i = prioridades; i < clientesEmFilas; i++)
+                {
+                    if (p1[i] != null)
                     {
-                        Console.WriteLine("Este ID é invalido, ou já está cadastrado. Por favor, escolha outro.");
+                        Console.WriteLine($"Paciente {i + 1}:");
+                        Console.WriteLine($"ID:{p1[i].id}");
+                        Console.WriteLine($"Nome:{p1[i].nome}");
+                        Console.WriteLine($"CPF:{p1[i].cpf}");
+                        Console.WriteLine($"RG:{p1[i].rg}");
                     }
-                
-               
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fila vazia!");
             }
         }
 
 
-
-
-        public string _Nome;
-        public string Nome
+        public void Atender()
         {
-            get
-            {
-                return _Nome;
-            }
-            set
-            {
-                
-                if (!string.IsNullOrEmpty(value) && !value.Any(char.IsDigit))
-                {
-                    _Nome = value;
-                }
-                else
-                {
-                    Console.WriteLine("Nome invalido! valores nulos e diferentes da formatação padrão não são aceitos.");
-                    _Nome = null;
-                }
-            }
 
-        }
-        public string _Cpf;
-        public string Cpf
-        {
-            get
+            if (clientesEmFilas > 0)
             {
-                return _Cpf;
-            }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value) && value.Length == 11)
-                {
-                    _Cpf = value;
-                }
-                else
-                {
-                    Console.WriteLine("Cpf invalido! valores nulos e diferentes da formatação padrão não são aceitos.");
-                }
-            }
-        }
-        public string _Rg;
-        public string Rg
-        {
-            get
-            {
-                return _Rg;
-            }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value) && value.Length == 9)
-                {
-                    _Rg = value;
-                } else
-                {
-                Console.WriteLine("Rg invalido! valores nulos e diferentes da formatação padrão não são aceitos.");
-                }
-            }
-        }
-        public DateTime _Nascimento;
-        public DateTime Nascimento 
-        {
-            get
-            {
-                return _Nascimento;
-            }
-            set
-            {
-                if (value <= DateTime.Now && value >= DateTime.Parse("1900-01-01"))
-                {
-                    _Nascimento = value;
-                }
-                else
-                {
-                    Console.WriteLine("Data de nascimento inválida!  valores nulos e diferentes da formatação padrão não são aceitos.");
-                }
-            }
-        }
 
+                Cliente Atendido = p1[0];
 
-        public void CadastrarCliente(string nome)
-        {
-            Console.WriteLine("\nDigite um id único para associar ao cliente: ");
-            idCliente = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nDigite seu nome: ");
-            nome = Console.ReadLine();
-            Cliente p1 = new Cliente;
-            Console.WriteLine("\nDigite seu CPF: ");
-            Cpf = Console.ReadLine();
-            Console.WriteLine("\nDigite seu RG: ");
-            Rg = Console.ReadLine();
-            
+                for (int j = 1; j < clientesEmFilas; j++)
+                {
+                    p1[j - 1] = p1[j];
+                }
 
-            bool dataValida = false;
-            while (!dataValida)
-            {
-                Console.WriteLine("\nDigite sua data de nascimento no formato dd/mm/aaaa: ");
-                if (DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime data))
-                {
-                   Nascimento = data;
-                    dataValida = true;
-                }
-                else
-                {
-                    Console.WriteLine("\nFormato de data inválido. Tente novamente.");
-                }
+                p1[clientesEmFilas - 1] = null;
+                clientesEmFilas--;
+
+                Console.WriteLine($"O Cliente {Atendido.nome} foi atendido com sucesso");
             }
-
-        }
-     
-        public void ConsultarCliente()
-        {
-            Console.WriteLine($"\nClientes\n {Nome}\n {Cpf}\n {Rg}\n{Nascimento}");
+            else
+            {
+                Console.WriteLine("Não existem pessoas a serem atendidas!");
+            }
         }
     }
 }
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,53 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace aplicativoBanco
+namespace Banco
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Cliente cliente1 = new Cliente();
-
             char opcao = '0';
+            Cliente cliente = new Cliente();
 
-            Console.WriteLine("\nBem vindo ao seu sistema de gerenciamento de bancos!\n\nEscolha oque deseja fazer: \n1 = Cadastrar cliente\n2 = Consultar cliente\n3 = Adicionar a fila\n4 = listar fila\nDigite 'q' para sair.\n");
+            Console.WriteLine("\nBom dia, boa tarde e boa noite, seja bem vindo ao seu sistema de gerenciamento de bancos!\n\nEscolha oque deseja fazer: \n1 = Cadastrar cliente\n2 = Consultar cliente\n3 = Adicionar clientes Prioritarios \n4 = Atender Pacientes\nDigite 'q' para sair.\n");
             while (opcao != 'q')
             {
                 opcao = Console.ReadKey().KeyChar;
-            
+
                 switch (opcao)
                 {
                     case '1':
-                        cliente1.CadastrarCliente();
-                    break;
-                    
+                        cliente.CadastrarCliente();
+                        break;
+
                     case '2':
-                        cliente1.ConsultarCliente();
-                    break;
+                        cliente.ListarClientes();
+                        break;
 
                     case '3':
-
-                    break;
+                        cliente.InserirPrioritarios();
+                        break;
 
                     case '4':
+                        cliente.Atender();
 
-                    break;
+                        break;
 
                     case 'Q':
                         Environment.Exit(1);
-                    break;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        break;
 
                 }
-                Console.WriteLine("Bem vindo ao seu sistema de gerenciamento de bancos!\n\nEscolha oque deseja fazer: \n1 = Cadastrar cliente\n2 = Consultar cliente\n3 = Adicionar a fila\n4 = listar fila\nDigite 'q' para sair.");
+                Console.WriteLine("Escolha oque deseja fazer: \n1 = Cadastrar cliente\n2 = Consultar cliente\n3 = Adicionar clientes Prioritarios \n4 = Atender Pacientes\nDigite 'q' para sair.\n");
 
             }
-
-
         }
     }
 }
